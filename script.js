1a8b48d934bb56e6b8cda93784b6e6151a86550a
Emilio Garza <emiliogarza07@gmail.com>
2018-06-15 22:17:44 -0700

    Made some progress on loading scripts only when applicable. Still have a few bugs to sort

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
+/* Var Defaults */
 var elems = document.querySelectorAll(".molecular__molecule");
 var currentSlide = 0;
+/* Something is wrong with this desktopJSLoaded Flad.. will have to investigate further tomorrow. */
+var desktopJSLoaded = false;
 var slideInterval = function() {
     nextMoleculeInterval = setInterval(nextMolecule, 5000);
 }
 
-elems.forEach(function(el,i) {
-  el.addEventListener("mouseover",function() {
-    appendContentToTheMiddleCircle(this.dataset.content);
-    goToMolecule(i);
-    clearInterval(nextMoleculeInterval);
-  });
-  el.addEventListener("mouseout", function() {
-    slideInterval();
-  });
-});
+init();
+window.addEventListener('resize', debounced);
 
-/*Get the slider going*/
-slideInterval();
+/*Controls For Desktop*/
+function loadDesktopBindings() {
+    elems.forEach(function(el,i) {
+        el.addEventListener("mouseover",function() {
+            appendContentToTheMiddleCircle(this.dataset.content);
+            goToMolecule(i);
+            clearInterval(nextMoleculeInterval);
+        });
+        el.addEventListener("mouseout", function() {
+            slideInterval();
+        });
+        el.addEventListener("click", function(e) {
+
+        });
+    });
+    slideInterval();
+    desktopJSLoaded = true;
+}
+function unLoadDesktopBindings() {
+    elems.forEach(function(el,i) {
+        el.removeEventListener("mouseover");
+        el.removeEventListener("mouseout");
+        el.removeEventListener("click");
+    });
+}
 
 function appendContentToTheMiddleCircle(content) {
     document.querySelectorAll(".molecular__innercircle")[0].innerHTML = content;
@@ -35,3 +53,41 @@ function goToMolecule(m) {
     elems[currentSlide].className = 'molecular__molecule molecular__molecule--selected';
     appendContentToTheMiddleCircle(elems[currentSlide].dataset.content);
 }
+
+function init() {
+    if (getComputedStyle(document.body).getPropertyValue("--device") == "desktop") {
+        console.log("Desktop")
+        if (desktopJSLoaded == false) {
+            console.log("DESKTOP, first time. Such a proud moment.");
+            loadDesktopBindings();
+        }
+    }
+    else {
+        console.log("You're mobile now son");
+        if (desktopJSLoaded) {
+            console.log("You used to be desktop, but you have now traded to the dark side...");
+            clearInterval(nextMoleculeInterval);
+            unLoadDesktopBindings();
+        }
+    }
+}
+
+/* For measuring window resize stuff */
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+}
+
+var debounced = debounce(function() {
+    init();
+}, 250);
