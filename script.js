c5c5d28784a8e7ff2227c2efb2921881aba6318d
Emilio Garza <emiliogarza07@gmail.com>
2018-06-15 20:26:41 -0700

    Made a rotator

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 var elems = document.querySelectorAll(".molecular__molecule");
+var currentSlide = 0;
+var slideInterval = function() {
+    nextMoleculeInterval = setInterval(nextMolecule, 5000);
+}
 
 elems.forEach(function(el,i) {
-  el.addEventListener("click",function() {
+  el.addEventListener("mouseover",function() {
     appendContentToTheMiddleCircle(this.dataset.content);
+    goToMolecule(i);
+    clearInterval(nextMoleculeInterval);
+  });
+  el.addEventListener("mouseout", function() {
+    slideInterval();
   });
 });
 
+/*Get the slider going*/
+slideInterval();
+
 function appendContentToTheMiddleCircle(content) {
-  document.querySelectorAll(".molecular__innercircle")[0].innerHTML = content;
+    document.querySelectorAll(".molecular__innercircle")[0].innerHTML = content;
+}
+
+function nextMolecule() {
+    elems[currentSlide].className = 'molecular__molecule';
+    currentSlide = (currentSlide + 1) % elems.length;
+    elems[currentSlide].className = 'molecular__molecule molecular__molecule--selected';
+    appendContentToTheMiddleCircle(elems[currentSlide].dataset.content);
+}
+
+function goToMolecule(m) {
+    elems[currentSlide].className = 'molecular__molecule';
+    currentSlide = (m + elems.length) % elems.length;
+    elems[currentSlide].className = 'molecular__molecule molecular__molecule--selected';
+    appendContentToTheMiddleCircle(elems[currentSlide].dataset.content);
 }
